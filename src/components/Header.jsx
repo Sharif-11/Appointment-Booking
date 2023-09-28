5c8eb2c990a63c004de064158824b1b15f338b9a
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-09-28 13:02:10 +0600

    route added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <div className="navbar flex justify-between px-9">
+            <div className="navbar flex justify-between">
                 <div className="navbar-start">
                     <NavLink to='/home' className="btn btn-ghost normal-case text-xl">MeetDoctor</NavLink>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal" >
-                        <li><NavLink to="/home">Home</NavLink></li>
-                        <li><NavLink to="/schedule">Schedule</NavLink></li>
-                        <li><NavLink to="/appointment">Appointment</NavLink></li>
-                        <li><NavLink to="/about">About</NavLink></li>
-                        <li><NavLink to="/login">Login</NavLink></li>
-                        <li><NavLink to="/admin">Admin</NavLink></li>
+                        <li className='mx-2'><NavLink to="/home">Home</NavLink></li>
+                        <li className='mx-2'><NavLink to="/schedule">Schedule</NavLink></li>
+                        <li className='mx-2'><NavLink to="/appointment">Appointment</NavLink></li>
+                        <li className='mx-2'><NavLink to="/about">About</NavLink></li>
+                        <li className='mx-2'><NavLink to="/login">Login</NavLink></li>
+                        <li className='mx-2'><NavLink to="/admin">Admin</NavLink></li>
                     </ul>
                 </div>
             </div>
