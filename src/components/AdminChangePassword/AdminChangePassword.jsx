0dcd9cc112dfecd8d166bb179d634148ea26d1c6
Shariful Islam <sharifsharifulislam061@gmail.com>
2023-10-08 21:29:51 +0600

    [Done] password change done

--- src/components/AdminChangePassword/AdminChangePassword.jsx
+++ src/components/AdminChangePassword/AdminChangePassword.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./AdminChangePassword.css"
 import "../AdminProfileSettings/AdminProfileSettings"
+import CustomForm from '../Formik/CustomForm';
+import passwordShema from '../../formValidator/password.yup'
+import CustomField from '../Formik/CustomField';
+import axios from 'axios'
+import {rootUrl} from '../../utils/rootUrl'
 const AdminChangePassword = () => {
+    const [message,setMessage]=useState(null)
+    const [status,setStatus]=useState(null)
+    const initialValues={
+        password:'',
+        confirmPassword:""
+    }
+    const handleSubmit=({password})=>{
+        setStatus(null);
+        setMessage(null);
+          axios.patch(rootUrl+'user/password',{password},{withCredentials:true})
+            .then(({data})=>{
+                 setStatus(data.status)
+                 data.status && setMessage(data.message)    
+            })
+            .catch((err)=>{
+              setStatus(false);
+              setMessage(err.response.data.message)
+            })
+    }
     return (
         <div className='card'>
-            <form>
+            {/* <form>
                 <div className='user-details'>
-                    {/* <div className='input-box'>
-                        <label htmlFor="Old_password">Old Password</label>
-                        <input type="password" id="old_password" placeholder="Give your old Password" className='bg-white' />
-                    </div> */}
+                   
 
                     <div className='input-box'>
                         <label htmlFor="New_password">New Password</label>
@@ -23,8 +44,35 @@ const AdminChangePassword = () => {
                 </div>
 
                 <button type="submit" className='btn glass bg-success text-white'>Save Changes</button>
-            </form>
+            </form> */}
+            <CustomForm
+               onSubmit={handleSubmit}
+               initialValues={initialValues}
+               validationSchema={passwordShema}
+            >
+                <CustomField
+                type='password'
+                name='password'
+                labelText={'Password'}
+                placeholder='Enter password'
+                className='input input-bordered'
+                />
+                <CustomField
+                type='password'
+                name='confirmPassword'
+                labelText={'Confirm Password'}
+                placeholder='Enter password'
+                className='input input-bordered'
+                />
+                <button type="submit" className='my-4 btn glass bg-success text-white'>Save Changes</button>
+                </CustomForm> 
+                {
+                    status ? <p className='text-[green] font-[600] text-md text-center'>*{message}</p>
+                      : <p className='text-[red] font-[600] text-md text-center'>*{message}</p>
+                }
+               
         </div>
+        
     );
 };
 
