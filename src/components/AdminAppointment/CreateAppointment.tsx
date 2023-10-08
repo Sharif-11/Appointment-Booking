653f7ebfe82a126f5837d69174334c3a2ffdb90e
Shariful Islam <sharifsharifulislam061@gmail.com>
2023-10-09 00:36:25 +0600

    [Done] appointment creation done

--- src/components/AdminAppointment/CreateAppointment.tsx
+++ src/components/AdminAppointment/CreateAppointment.tsx
@@ -2,9 +2,11 @@ import React,{useState,useEffect} from 'react';
 import Slot from '../Slot/Slot';
 import axios from 'axios'
 import {rootUrl} from '../../utils/rootUrl'
+import {useNavigate} from 'react-router-dom'
 const CreateAppointment = () => {
     const [slots,setSlots]=useState([])
     const [loading,setLoading]=useState(true);
+    const navigate=useNavigate()
     useEffect(()=>{
         setLoading(true)
       axios.post(rootUrl+'doctor/slots',{},{withCredentials:true})
@@ -12,12 +14,21 @@ const CreateAppointment = () => {
            .catch(()=>setSlots([]))
     setLoading(false)
     },[])
+    const createAppointment=async (id)=>{
+await axios.post(rootUrl+'doctor/appointment',{slotId:id},{withCredentials:true})
+     .then(({data})=>{
+            if(data.status){
+                 navigate('/admin/admin-appointment/start-close')
+
+            }
+     })
+    }
     return (
         <div>
             <p className='text-center font-[700] text-xl'>Slots Available today</p>
           <div className='flex justify-between flex-wrap mx-8'>
             {
-                slots.map((slot,idx)=><Slot id={idx} {...slot}/>)
+                slots.map((slot,idx)=><Slot id={idx} {...slot} createAppointment={()=>createAppointment(slot.id)}/>)
             }
           </div>
         </div>
--- src/components/Slot/Slot.tsx
+++ src/components/Slot/Slot.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Slot = ({startTime,endTime,bookingStartTime,bookingEndTime,isStart=true}) => {
+const Slot = ({startTime,endTime,bookingStartTime,bookingEndTime,isStart=true,createAppointment}) => {
     return (
         <div className='card my-3 shadow-xl w-[40%]'>
             <p className='font-[700] text-lg'>Service Time</p>
             <p className='text-md text-[green]  italic'>{startTime} - {endTime}</p>
-            <p className='font-[700] text-lg's>Booking Time</p>
+            <p className='font-[700] text-lg'>Booking Time</p>
             <p className='text-md italic text-[green]'>{bookingStartTime} - {bookingEndTime}</p>
-            <button disabled={!isStart} className='btn btn-info mt-5'>Create Appointment</button>
+            <button disabled={!isStart} className='btn btn-info mt-5'
+            onClick={createAppointment}
+            >Create Appointment</button>
         </div>
     );
 };
