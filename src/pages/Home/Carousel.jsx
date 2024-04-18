671aa9ebdaa0bbc998a125671658f575aed50e49
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-04-18 22:35:27 +0600

    doctor info added at home

--- src/pages/Home/Carousel.jsx
+++ src/pages/Home/Carousel.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './Carousel.css'; // Import your CSS file for styling
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "../../Contexts/UserContext";
+import "./Carousel.css"; // Import your CSS file for styling
 
 const images = [
   "/public/images/Banner-1.jpg",
@@ -11,7 +12,7 @@ const images = [
 
 const Carousel = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
+  const { doctorInfo } = useContext(UserContext);
   useEffect(() => {
     // Automatically advance to the next image every 3 seconds
     const interval = setInterval(() => {
@@ -25,23 +26,29 @@ const Carousel = () => {
 
   return (
     <div className="carousel-container bg-gray-900 relative h-[650px] w-full">
-        {images.map((image, index) => (
-          <div key={index} className='opacity-40'>
-            <img
-              src={image}
-              alt={`Image ${index + 1}`}
-              className={`carousel-image absolute top-0 left-0 w-full h-full opacity-0${index === currentImageIndex ? 'active' : ''
-                }`}
-            />
-          </div>
-        ))}
+      {images.map((image, index) => (
+        <div key={index} className="opacity-40">
+          <img
+            src={image}
+            alt={`Image ${index + 1}`}
+            className={`carousel-image absolute top-0 left-0 w-full h-full opacity-0${
+              index === currentImageIndex ? "active" : ""
+            }`}
+          />
+        </div>
+      ))}
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center">
-          <h1 className="font-bold py-3 text-white">Hello, I am Dr. X</h1>
-          <p className='py-3 text-white'>
-            Book an appointment with our esteemed cardiologist today. <br /> Explore Dr. X's renowned Cardiology Practice.
-          </p>
-          <Link to="/appointment" className='btn bg-success glass text-white font-bold focus:outline-none'>Get an Appointment</Link>
+          <h1 className="font-bold py-3 text-white">
+            Hello, I am {doctorInfo?.name}
+          </h1>
+          <p className="py-3 text-white">{doctorInfo?.aboutMe}</p>
+          <Link
+            to="/appointment"
+            className="btn bg-success glass text-white font-bold focus:outline-none"
+          >
+            Get an Appointment
+          </Link>
         </div>
       </div>
     </div>
--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -1,141 +1,136 @@
-import About from '../pages/About/About';
-import Admin from '../pages/Admin/Admin';
-import Layout from '../components/Layout';
-import Home from '../pages/Home/Home';
-import Login from '../pages/Login/Login';
-import Signup from '../pages/Signup/Signup';
-import Contact from '../components/Contact';
-import PatientQueue from '../components/PatientQueue/PatientQueue';
-import ErrorPage from '../components/ErrorPage/ErrorPage'
-import AdminDashboard from '../components/AdminDashboard';
-import AdminScheduleTimings from '../components/AdminScheduleTimings/AdminScheduleTimings';
-import AdminProfileSettings from '../components/AdminProfileSettings/AdminProfileSettings';
-import AdminChangePassword from '../components/AdminChangePassword/AdminChangePassword';
-import Schedule from '../pages/Schedule/Schedule';
-import UserProfile from '../components/UserProfile/UserProfile';
-import AdminAppointment from '../components/AdminAppointment/AdminAppointment';
-import CreateAppointment from '../components/AdminAppointment/CreateAppointment';
-import DeleteAppointment from '../components/AdminAppointment/DeleteAppointment';
-import StartAppointment from '../components/AdminAppointment/StartAppointment';
-import CloseAppointment from '../components/AdminAppointment/CloseAppointment';
-import Appointments from '../pages/Appointment/Appointments';
+import AdminAppointment from "../components/AdminAppointment/AdminAppointment";
+import CloseAppointment from "../components/AdminAppointment/CloseAppointment";
+import CreateAppointment from "../components/AdminAppointment/CreateAppointment";
+import DeleteAppointment from "../components/AdminAppointment/DeleteAppointment";
+import StartAppointment from "../components/AdminAppointment/StartAppointment";
+import AdminChangePassword from "../components/AdminChangePassword/AdminChangePassword";
+import AdminDashboard from "../components/AdminDashboard";
+import AdminProfileSettings from "../components/AdminProfileSettings/AdminProfileSettings";
+import AdminScheduleTimings from "../components/AdminScheduleTimings/AdminScheduleTimings";
+import Contact from "../components/Contact";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Layout from "../components/Layout";
+import PatientQueue from "../components/PatientQueue/PatientQueue";
+import UserProfile from "../components/UserProfile/UserProfile";
+import Admin from "../pages/Admin/Admin";
+import Appointments from "../pages/Appointment/Appointments";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Schedule from "../pages/Schedule/Schedule";
+import Signup from "../pages/Signup/Signup";
 
-import PatientProfile from '../components/PatientProfile/PatientProfile';
-import UserProfileSettings from '../components/UserProfileSettings'
-import PaymentCheckout from '../components/PaymentCheckout';
+import PatientProfile from "../components/PatientProfile/PatientProfile";
+import PaymentCheckout from "../components/PaymentCheckout";
+import UserProfileSettings from "../components/UserProfileSettings";
 const mainRouter = [
-    {
-        path: '/',
-        element: <Layout></Layout>,
-        children: [
-            {
-                path: "",
-                element: <Home></Home>
-            },
-            {
-                path: "/home",
-                element: <Home></Home>
-            },
-            {
-                path: "/about",
-                element: <About></About>
-            },
-            {
-                path: "/",
-                element: <Admin></Admin>,
-                children: [
-                    {
-                        path: "/admin",
-                        element: <AdminDashboard></AdminDashboard>
-                    },
-                    {
-                        path: "admin/dashboard",
-                        element: <AdminDashboard></AdminDashboard>
-                    },
-                    {
-                        path: "admin/admin-schedule-timings",
-                        element: <AdminScheduleTimings></AdminScheduleTimings>
-                    },
-                    {
-                        path: "admin/admin-appointment",
-                        element: <AdminAppointment />,
-                        children: [
-                            {
-                                path: '',
-                                element: <CreateAppointment />
-                            },
-                            {
-                                path: 'create',
-                                element: <CreateAppointment />
-                            },
-                            {
-                                path: 'delete',
-                                element: <DeleteAppointment />
-                            },
-                            {
-                                path: 'start',
-                                element: <StartAppointment />
-                            },
-                            {
-                                path: 'close',
-                                element: <CloseAppointment />
-                            },
-                        ]
-                    },
-                    {
-                        path: "admin/admin-profile-settings",
-                        element: <AdminProfileSettings></AdminProfileSettings>
-                    },
-                    {
-                        path: "admin/admin-change-password",
-                        element: <AdminChangePassword></AdminChangePassword>
-                    }
-                ]
-            },
-            {
-                path: "/appointment",
-                element: <Appointments/>
-            },
-            {
-                path: "/checkout/:id",
-                element: <PaymentCheckout/>
-            },
-            {
-                path: "/contact",
-                element: <Contact></Contact>
-            },
-            {
-                path: "/schedule",
-                element: <Schedule></Schedule>
-            },
-            {
-                path: "/login",
-                element: <Login></Login>
-            },
-            {
-                path: "/signup",
-                element: <Signup></Signup>
-            },
-            {
-                path: "/user-profile",
-                element: <UserProfile></UserProfile>
-            },
-            {
-                path: "/patient-profile",
-                element: <PatientProfile></PatientProfile>
-            },
-            {
-                path: "/user-profile-settings",
-                element: <UserProfileSettings></UserProfileSettings>
-            },
-            {
-                path: "/patient-queue",
-                element: <PatientQueue></PatientQueue>
-            }
-        ],
-        errorElement: <ErrorPage></ErrorPage>
-    }
-];
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
 
+      {
+        path: "/",
+        element: <Admin></Admin>,
+        children: [
+          {
+            path: "/admin",
+            element: <AdminDashboard></AdminDashboard>,
+          },
+          {
+            path: "admin/dashboard",
+            element: <AdminDashboard></AdminDashboard>,
+          },
+          {
+            path: "admin/admin-schedule-timings",
+            element: <AdminScheduleTimings></AdminScheduleTimings>,
+          },
+          {
+            path: "admin/admin-appointment",
+            element: <AdminAppointment />,
+            children: [
+              {
+                path: "",
+                element: <CreateAppointment />,
+              },
+              {
+                path: "create",
+                element: <CreateAppointment />,
+              },
+              {
+                path: "delete",
+                element: <DeleteAppointment />,
+              },
+              {
+                path: "start",
+                element: <StartAppointment />,
+              },
+              {
+                path: "close",
+                element: <CloseAppointment />,
+              },
+            ],
+          },
+          {
+            path: "admin/admin-profile-settings",
+            element: <AdminProfileSettings></AdminProfileSettings>,
+          },
+          {
+            path: "admin/admin-change-password",
+            element: <AdminChangePassword></AdminChangePassword>,
+          },
+        ],
+      },
+      {
+        path: "/appointment",
+        element: <Appointments />,
+      },
+      {
+        path: "/checkout/:id",
+        element: <PaymentCheckout />,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/schedule",
+        element: <Schedule></Schedule>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/user-profile",
+        element: <UserProfile></UserProfile>,
+      },
+      {
+        path: "/patient-profile",
+        element: <PatientProfile></PatientProfile>,
+      },
+      {
+        path: "/user-profile-settings",
+        element: <UserProfileSettings></UserProfileSettings>,
+      },
+      {
+        path: "/patient-queue",
+        element: <PatientQueue></PatientQueue>,
+      },
+    ],
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+];
 
 export default mainRouter;
